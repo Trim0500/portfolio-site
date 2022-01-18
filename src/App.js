b5c2a626bb2498c59ed1bc240766338482a5f344
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-18 15:16:14 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  const faunadb = require('faunadb');
+  const q = faunadb.query;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const secret = process.env.FAUNADB_SECRET;
+  var endpoint = process.env.FAUNA_ENDPOINT;
+
+  if(typeof secret === 'undefined' || secret === '') {
+    console.error('The FAUNADB_SECRET environment variable is not set, exiting.');
+    process.exit(1);
+  }
+
+  if(!endpoint) endpoint = 'https://db.fauna.com';
+
+  var mg, domain, port, scheme;
+  if((mg = endpoint.match(/^(https?):\/\/([^:]+)(:(\d+))?/))) {
+    scheme = mg[1] || 'https';
+    domain = mg[2] || 'db.fauna.com';
+    port = mg[4] || 443;
+  }
+
+  const client = new faunadb.Client({
+    secret: secret,
+    domain: domain,
+    port: port,
+    scheme: scheme
+  })
+
+  client.query(
+    q.CreateCollection({name: 'myCollection'})
+  )
+    .then((ret) => console.log(ret))
+    .catch((err) => console.error('Error: %s', err))
 }
-
-export default App;
