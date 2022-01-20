16fca2658364463131f6fad09d737eff30787229
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-20 18:07:28 -0500

    Figured out how to post to fauna, changed the prefix of the env var

--- src/db/test_db.js
+++ src/db/test_db.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 export default function TestDB() {
+    require('dotenv').config();
+
     var faunadb = require('faunadb'),
     q = faunadb.query
 
     var client = new faunadb.Client({
-        secret: process.env.FAUNA_ADMIN_KEY,
+        secret: process.env.REACT_APP_FAUNA_ADMIN_KEY,
         domain: 'db.fauna.com',
         port: 443,
         scheme: 'https',
     })
-
-    console.log(client)
 
     var CreateP = client.query(
         q.Create(
