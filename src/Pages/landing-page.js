89c27a61247c765c3b7b812711d208bdec246173
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-25 14:38:59 -0500

    Implemented ReactBootstrap

--- src/Pages/landing-page.js
+++ src/Pages/landing-page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import '../css/styles.css'
 
 export default class Homepage extends React.Component {
@@ -14,29 +15,37 @@ export default class Homepage extends React.Component {
 
     render() {
         return(
-            <div name="Content">
-                <div name="Image_Div">
-                    <img name="PFP" src={this.PFPImage} alt="PFP by Fireplace" width="631px" height="631" />
-                </div>
-                <div name="Header_Content">
-                    <h1>Welcome to Trim's Space!</h1>
-                    <p>{process.env.REACT_APP_LANDING_PAGE_INTRO}</p>
-                </div>
-                <div name="Page_Thumbnails">
-                    <div name="Thumbnail_Pictures">
-                        <img name="Hobbies_Img" src={this.HobbiesImage} alt="Hobbies Thumbnail" width="350px" height="350px" />
-                        <img name="Academics_Img" src={this.AcademicsImage} alt="Academics Thumbnail" width="350px" height="350px" />
-                        <img name="Contact_Img" src={this.ContactImage} alt="Contacts Thumbnail" width="350px" height="350px" />
-                    </div>
-                </div>
-                <div name="Page_Thumbnails">
-                    <div name="Thumbnail_Pictures">
-                        <a name="Hobbies_Link" href='/'>Let me Tell you About What I Like!</a>
-                        <a name="Academics_Link" href='/'>Check Out What I've Learned!</a>
-                        <a name="Contact_Link" href='/'>Let's Get in Touch!</a>
-                    </div>
-                </div>
-            </div>
+            <Container name="Content">
+                <Row name="Image_Div">
+                    <Col sm={4} name="PFP_Div"><img name="PFP" src={this.PFPImage} alt="PFP by Fireplace" width="631px" height="631" /></Col>
+                    <Col sm={8} name="Header_Div">
+                        <h1>Welcome to Trim's Space!</h1>
+                        <p>{process.env.REACT_APP_LANDING_PAGE_INTRO}</p>
+                        <Row>
+                            <Col>
+                                <img name="Hobbies_Img" src={this.HobbiesImage} alt="Hobbies Thumbnail" width="350px" height="350px" />
+                            </Col>
+                            <Col>
+                                <img name="Academics_Img" src={this.AcademicsImage} alt="Academics Thumbnail" width="350px" height="350px" />
+                            </Col>
+                            <Col>
+                                <img name="Contact_Img" src={this.ContactImage} alt="Contacts Thumbnail" width="350px" height="350px" />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <a name="Hobbies_Link" href='/'>Let me Tell you About What I Like!</a>
+                            </Col>
+                            <Col>
+                                <a name="Academics_Link" href='/'>Check Out What I've Learned!</a>
+                            </Col>
+                            <Col>
+                                <a name="Contact_Link" href='/'>Let's Get in Touch!</a>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './Pages/landing-page';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Homepage />,
