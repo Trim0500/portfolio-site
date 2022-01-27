b5d2ab2981a98c2d303db5cfd69da1e72c6ac0bf
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-26 20:52:20 -0500

    Established routing for pages

--- src/Pages/contact-page.js
+++ src/Pages/contact-page.js
@@ -3,10 +3,6 @@ import { Container } from 'react-bootstrap';
 import NavBar from '../Fragment/navbar';
 
 export default class ContactPage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <Container>
