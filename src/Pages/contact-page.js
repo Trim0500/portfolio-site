dc2067c5734c1eea108f0937712aa43859cac51d
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-27 10:22:58 -0500

    Created email submission functionality

--- src/Pages/contact-page.js
+++ src/Pages/contact-page.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import NavBar from '../Fragment/navbar';
-import{ init } from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 
 export default class ContactPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            EmailJs: init(process.env.REACT_APP_EMAIL_USER_ID)
-        }
+        require('dotenv').config();
+        this.form = React.createRef();
         this.HandleSubmit = this.HandleSubmit.bind(this);
     }
 
     HandleSubmit(e) {
         e.preventDefault();
+
+        emailjs.sendForm('service_ttaz1gh', 'template_3nfgg25', this.form.current)
+        .then((res) => {
+            console.log('Success!', res.status, res.text);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
     }
 
     render() {
@@ -24,9 +31,9 @@ export default class ContactPage extends React.Component {
                     <h1>Let's Get in Touch!</h1>
                     <Row>
                         <Col>
-                            <form onSubmit={this.HandleSubmit}>
-                                <input placeholder='Enter your name here...' className='form-control' type="text" name="user_name" /><br/>
-                                <input placeholder='Enter your email here...' className='form-control' type="email" name="user_email" /><br/>
+                            <form ref={this.form} onSubmit={this.HandleSubmit}>
+                                <input placeholder='Enter your name here...' className='form-control' type="text" name="from_name" /><br/>
+                                <input placeholder='Enter your email here...' className='form-control' type="email" name="reply_to" /><br/>
                                 <textarea placeholder='What are you writing for?' className='form-control' name="message" rows="15" /><br/>
                                 <input className="btn btn-primary" type="submit" value="Send" />
                             </form>
