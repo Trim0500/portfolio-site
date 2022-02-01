1883bdbee4bca9f4e211087ed082561f3f0c10ad
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-02-01 13:58:54 -0500

    Place container for page

--- src/Pages/professional-page.js
+++ src/Pages/professional-page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import NavBar from '../Fragment/navbar';
 
 export default class ProfessionalPage extends React.Component {
@@ -7,6 +7,17 @@ export default class ProfessionalPage extends React.Component {
         return (
             <Container>
                 <NavBar />
+                <Container className='container-content'>
+                    <h1 style={{paddingBottom: "0.5em", paddingTop: "0.5em"}}>Here's What I've Done for a Living!</h1>
+                    <Row>
+                        <Col>
+                            <h1>CV goes here</h1>
+                        </Col>
+                        <Col>
+                            <h1>Place details about particular experiences here</h1>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         )
     }
