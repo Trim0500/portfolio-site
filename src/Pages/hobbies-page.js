f697adbb47c39cc2ead458a26779f7ced69d0e9e
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-02-03 09:40:51 -0500

    Provided container, changed p tag styling, changed landing page a tag styling

--- src/Pages/hobbies-page.js
+++ src/Pages/hobbies-page.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import NavBar from '../Fragment/navbar';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default class HobbiesPage extends React.Component {
     render() {
         return (
             <Container>
                 <NavBar />
+                <Container className='container-content'>
+                    <h1 style={{paddingBottom: "0.5em", paddingTop: "0.5em"}}>My hobbies: Analysis, Editing &amp; More!</h1>
+                    <Row>
+                        <Col>
+                            <h3>Edits</h3>
+                            <p>Text about edits will go here</p>
+                        </Col>
+                        <Col>
+                            <h1>Images will go here</h1>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         )
     }
