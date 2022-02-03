f110c4c2cb8da91f877bf7b22bce08dcefed2ee4
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-02-03 10:38:45 -0500

    Can embed vids

--- src/Pages/hobbies-page.js
+++ src/Pages/hobbies-page.js
@@ -27,7 +27,8 @@ export default class HobbiesPage extends React.Component {
                             <p>If you like the samples embedded here, check out the <a className='p-contact-link' href='/contact'>contact page</a> to find my channel!</p>
                         </Col>
                         <Col sm={7}>
-                            <h1>Video Embeds will go here</h1>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/wwiHrH4ebeM?controls=0&amp;start=757" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/ak2lV5cVdP8?controls=0&amp;start=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </Col>
                     </Row>
                     <Row>
