cdc418f609548797b0dad8707ac1569755354d17
Trim0500 <tristanblacklafleur@hotmail.ca>
2022-05-01 14:10:46 -0400

    Chore[Portfolio-14]: Moved the hobbies text to resource file

--- src/resources/text_resources_en.js
+++ src/resources/text_resources_en.js
@@ -7,6 +7,42 @@ export const text_resources = {
             HOME_ACADEMICS_TEXT: "Check Out What I've Learned!",
             HOME_CONTACT_TEXT: "Let's Get in Touch!"
         },
+        HOBBIES: {
+            QUICK_LINKS: "Quick Links: ",
+            EDITS_LINK: "Edits",
+            VIDEOS_LINK: "Videos",
+            LANDSCAPES_LINK: "Landscapes",
+            HOBBIES_HEADER: "My hobbies: Analysis, Editing & More!",
+            EDITS_INTRO_TEXT: "Whenever I watch media, play it, make weird connections in my head or I get inspired I try my hand at creating good backgrounds and thumbnails or I make silly memes based off that media." +
+                            "The former of which are usually for my own purposes whether it’s cuz I wanted my own custom background for my phone and tablet or to make an appealing image for a video I had planned.",
+            EDITS_EXAMPLE_TEXT: "The later examples of memes I make are usually to emphasize something I encountered in my life and made a connection to another property as most memes tend to be." +
+                                "These I tend to share with my friends but elect to not post online since I prefer to observe what others do on the Internet rather than directly engage with it.",
+            EDITS_FUN_FACT_TEXT: "Fun Fact: I made all of these edits on PowerPoint! It's lowkey a really good budget photoshop option!",
+            EDITS_NUTBALL_TEXT: "Nutball do be wild.",
+            EDITS_NM_TEXT: "I make the weirdest connections...",
+            EDITS_FEH_TEXT: "Fire Emblem Heroes iPad background which emphasizes my best units!",
+            EDITS_BLOODY_PALACE_TEXT: "This is one of the thumbnails to my first DMC5 Bloody Palace clear on YT!",
+            VIDEOS_INTRO_TEXT: "I took to making videos on YouTube around the time of Summer of 2020." +
+                            "You’ll notice that lines up with the first Summer in the depths of the Covid pandemic, which meant that I didn’t have much to do besides work." +
+                            "So I quickly got bored and decided to try my hand at a field I’ve always wanted to try.",
+            VIDEOS_EXAMPLES_TEXT: "I tend to make more analytical style videos that are somewhat long to half-hour ventures and they usually cover what else but my favorite hobby, games!" +
+                                "I’ve learned a great deal about the creation process of making videos and editing it into a clear vision brought to life!" +
+                                "Because I like to play games more than talk about them and stay busy with School and work, I try to tackle a single big project rather than try to pump out a large quantity at a time so there’s only so much to comb through.",
+            VIDEOS_LINK_TEXT_1: "If you like the samples embedded here, check out the ",
+            VIDEOS_LINK_TEXT_2: "contact page",
+            VIDEOS_LINK_TEXT_3: " to find my channel!",
+            VIDEOS_ECHOES_TEXT: "One of my most recent & personal favorite analysis videos! Strap in though, it's a long one!",
+            VIDEOS_MEME_TEXT: "I thought this moment in Fire Emblem Binding Blade was funny!",
+            LANDSCAPES_OVERVIEW_TEXT: "When I go biking for a while or just explore around my neighborhood outside, I tend to encounter a lot of beautiful scenery and landscapes that compels me to try and capture in photo form." +
+                                    "I’m not entirely sure why I like doing this so much, I guess it just happens on instinct for me." +
+                                    "Either way I’ve seen a lot over the years and I thought I’d show some particular highlights here!",
+            LANDSCAPES_GASPESIE_TEXT: "A cool shot looking out into a river at Gaspésie",
+            LANDSCAPES_WINTER_TEXT: "Winter makes for gourgeous sunsets!",
+            LANDSCAPES_FLOWER_TEXT: "Got a neat close-up w/background being blurred out!",
+            LANDSCAPES_PORTRAIT_TEXT: "I thought the top of the mountain would make for a great profile shot.",
+            HOBBIES_BACKTOP_TEXT_1: "Back to ",
+            HOBBIES_BACKTOP_TEXT_2: "top"
+        },
         PROFESSIONAL: {
             INTERNSHIP_HEADING: "NexOne Internship",
             INTERNSHIP_OVERVIEW: "From March 2022 to May 2022 I was taken on as an intern at NexOne Real Estate Solutions. " +
@@ -28,6 +64,34 @@ export const text_resources = {
             HOME_HOBBIES_TEXT: "",
             HOME_ACADEMICS_TEXT: "",
             HOME_CONTACT_TEXT: ""
+        },
+        HOBBIES: {
+            QUICK_LINKS: "",
+            EDITS_LINK: "",
+            VIDEOS_LINK: "",
+            LANDSCAPES_LINK: "",
+            HOBBIES_HEADER: "",
+            EDITS_INTRO_TEXT: "",
+            EDITS_EXAMPLE_TEXT: "",
+            EDITS_FUN_FACT_TEXT: "",
+            EDITS_NUTBALL_TEXT: "",
+            EDITS_NM_TEXT: "",
+            EDITS_FEH_TEXT: "",
+            EDITS_BLOODY_PALACE_TEXT: "",
+            VIDEOS_INTRO_TEXT: "",
+            VIDEOS_EXAMPLES_TEXT: "",
+            VIDEOS_LINK_TEXT_1: "",
+            VIDEOS_LINK_TEXT_2: "",
+            VIDEOS_LINK_TEXT_3: "",
+            VIDEOS_ECHOES_TEXT: "",
+            VIDEOS_MEME_TEXT: "",
+            LANDSCAPES_OVERVIEW_TEXT: "",
+            LANDSCAPES_GASPESIE_TEXT: '',
+            LANDSCAPES_WINTER_TEXT: "",
+            LANDSCAPES_FLOWER_TEXT: "",
+            LANDSCAPES_PORTRAIT_TEXT: "",
+            HOBBIES_BACKTOP_TEXT_1: "",
+            HOBBIES_BACKTOP_TEXT_2: ""
         },
         PROFESSIONAL: {
             INTERNSHIP_HEADING: "",
